9e95dd771388eb4e528e8ff28d6296a92cd87067
cmyk <schelob@protonmail.com>
2025-04-05 13:21:46 +0200

    version = "6.12.20-rtc-slim";

--- overlays/default.nix
+++ overlays/default.nix
@@ -26,7 +26,7 @@ let
     };
     v6_12_20 = {
       src = rpi-linux-6_12_20-src;
-      version = "6.12.21";
+      version = "6.12.20-rtc-slim";
     };
   };
   boards = [ "bcm2711" "bcm2712" ];
